28e28466ef15f430e4b2da56a87e43a92cd43611
tgood111 <tgood111@live.com>
2017-09-07 19:33:12 -0500

    Added Exception Day Model

---
+++ TrashPickUp/Models/ExceptionDays.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickUp.Models
+{
+    public class ExceptionDays
+    {
+        public Guid Id { get; set; }
+        public Guid User { get; set; }
+        public DateTime Exception { get; set; }
+    }
+}
