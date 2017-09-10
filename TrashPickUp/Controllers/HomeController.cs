a8d5a6532918e5a48a57c959a4d3b9fc6ad8e27c
tgood111 <tgood111@live.com>
2017-09-10 17:29:26 -0500

    Figured out how to set default day.

--- TrashPickUp/Controllers/HomeController.cs
+++ TrashPickUp/Controllers/HomeController.cs
@@ -3,12 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
 using TrashPickUp.Models;
 
 namespace TrashPickUp.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Schedule()
+        {
+            var dbContext = new ApplicationDbContext();
+            var model = new ScheduleViewModel();
+            var guid = User.Identity.GetUserId();
+            model.Exceptions = dbContext.ExceptionDays.Where(x => x.User.ToString() == guid).ToList();
+            model.VacationPeriods = dbContext.VacationPeriods.Where(x => x.User.ToString() == guid).ToList();
+            model.DefaultDay = dbContext.DefaultTrashSchedules.FirstOrDefault(x => x.User.ToString() == guid);
+            return View(model);
+        }
+
+        [HttpPost]
+        public void DefaultScheduleForm(string DateChoice)
+        {
+            var guid = User.Identity.GetUserId();
+            int value = 0;
+            if (DateChoice == "Sunday")
+            {
+                value = 0;
+            }
+            if (DateChoice == "Monday")
+            {
+                value = 1;
+            }
+            if (DateChoice == "Tuesday")
+            {
+                value = 2;
+            }
+            if (DateChoice == "Wednesday")
+            {
+                value = 3;
+            }
+            if (DateChoice == "Thursday")
+            {
+                value = 4;
+            }
+            if (DateChoice == "Friday")
+            {
+                value = 5;
+            }
+            if (DateChoice == "Saturday")
+            {
+                value = 6;
+            }
+
+            using (var db = new ApplicationDbContext())
+            {
+                var result = db.DefaultTrashSchedules.SingleOrDefault(b => b.User.ToString() == guid);
+                if (result != null)
+                {
+                    result.Day = (DayOfWeek) value;
+                }
+                else
+                {
+                    DefaultTrashSchedule d = new DefaultTrashSchedule();
+                    d.User = new Guid(guid);
+                    d.Day = (DayOfWeek) value;
+                    db.DefaultTrashSchedules.Add(d);
+                }
+                db.SaveChanges();
+            }
+        }
+
         public ActionResult Navbar()
         {
             var model = new NavBarViewModel()
---
+++ TrashPickUp/Models/ScheduleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrashPickUp.Models
+{
+    public class ScheduleViewModel
+    {
+        public List<ExceptionDay> Exceptions { get; set; }
+        public List<VacationPeriod> VacationPeriods { get; set; }
+        public DefaultTrashSchedule DefaultDay { get; set; }
+        public List<string> Days { get; set; }
+
+        public ScheduleViewModel()
+        {
+            Days = new List<string>();
+            Days.Add("Sunday");
+            Days.Add("Monday");
+            Days.Add("Tuesday");
+            Days.Add("Wednesday");
+            Days.Add("Thursday");
+            Days.Add("Friday");
+            Days.Add("Saturday");
+        }
+    }
+}
