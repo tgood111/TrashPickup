c926c573209a7e95c3b4c05fcf7b7f6360dc025c
tgood111 <tgood111@live.com>
2017-09-10 10:10:14 -0500

    Created a model for is admin or user in the navbar

---
+++ TrashPickUp/Models/NavBarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickUp.Models
+{
+    public class NavBarViewModel
+    {
+        public bool Administrator { get; set; }
+        public bool User { get; set; }
+    }
+}
