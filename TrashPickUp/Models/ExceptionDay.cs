d1043db34287a6c6dcc033e549872adacc56feb4
tgood111 <tgood111@live.com>
2017-09-10 10:09:35 -0500

    Created Exception Day Class

--- TrashPickUp/Models/ExceptionDay.cs
+++ TrashPickUp/Models/ExceptionDay.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace TrashPickUp.Models
 {
-    public class ExceptionDays
+    public class ExceptionDay
     {
+        [Key]
         public Guid Id { get; set; }
         public Guid User { get; set; }
         public DateTime Exception { get; set; }
+
+        public virtual List<ExceptionDay> ExceptionDays { get; set; }
     }
 }
