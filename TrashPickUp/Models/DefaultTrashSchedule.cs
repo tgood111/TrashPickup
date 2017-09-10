f5ebfe36d1ee1e389d7281ddd5e76a901b5a0a21
tgood111 <tgood111@live.com>
2017-09-10 10:09:25 -0500

    Creeated default trash schedule class

---
+++ TrashPickUp/Models/DefaultTrashSchedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickUp.Models
+{
+    public class DefaultTrashSchedule
+    {
+        [Key]
+        public Guid User { get; set; }
+        public DayOfWeek Day { get; set; }
+
+        public virtual List<DefaultTrashSchedule> DefaulTrashSchedules { get; set; }
+    }
+}
