097cd5ea9266e96fcd8f5130ee931a0889537404
tgood111 <tgood111@live.com>
2017-09-10 16:44:27 -0500

    Revert "Working on Schedule Controller"
    This reverts commit dcfaefb18a9488f826a2d680546e5988fd319cba.

--- TrashPickUp/Controllers/HomeController.cs
+++ TrashPickUp/Controllers/HomeController.cs
@@ -24,11 +24,6 @@ namespace TrashPickUp.Controllers
             return View();
         }
 
-        public ActionResult Schedule()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
