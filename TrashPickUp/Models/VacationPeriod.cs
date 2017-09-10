1d0212fcc02435bca9a27b3093fefde283b094d2
tgood111 <tgood111@live.com>
2017-09-10 10:09:56 -0500

    Added Vacation Period Class

---
+++ TrashPickUp/Models/VacationPeriod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickUp.Models
+{
+    public class VacationPeriod
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid User { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public virtual List<DateTime> VacationPeriods { get; set; }
+    }
+}
